74f55b6443eb44cb1fd2e5d17e38fa49bad8ed8c
Marco Garcia <marcogarcia831@gmail.com>
2021-03-20 15:39:43 -0700

    ETL COMPLETED

--- Cleaner.js
+++ Cleaner.js
@@ -1,132 +1,191 @@
 const csv = require('csv-parser');
-const fs = require('fs')
+const fs = require('fs');
+const readline = require('readline');
 const products = [];
 const styles = [];
 const failed = [];
-const results2 = [];
+const ids = [];
 
-//for products all should be string
+const urlPattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+    '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
 
-fs.createReadStream('./DataBase/product.csv').pipe(csv({})).on('data', (data)=>{
-
-  var str = '';
-  var boolean = true
-  for(var key in data){
-    var value = data[key]
-    if(key === ' default_price'){
-      var number = parseInt(value)
-      if(isNaN(number)){
-      var number = value.match(/[0-9]+/g);
-      value = number
-      }
-    }
-    if(boolean){
-      if(key !== ' default_price' && key !== 'id' ){
-        str += '"'+value+'"';
-        boolean = false;
-      }else{
-        str += value;
-    boolean = false;
-      }
-
-  }else {
-    if(key !== ' default_price' && key !== 'id' ){
-      str += ',"' + value +'"';
-    }else {
-      str += ',' + value;
-    }
-  }
+function isValidURL(url) {
+  let str = url;
+  // remove surrounding double quotes
+  if (url[0] === '"' && url[0] === url[url.length - 1]) {
+    str = url.substring(1, url.length - 1);
   }
 
-  fs.appendFile('test.csv',str + '\n',(err)=>{
-    if(err){
-      console.log(err)
-    }else{
+  return !!urlPattern.test(str);
+}
 
-    }
-    })
-}).on('end',()=>{
-  console.log('completed')
-})
+// const readPhotos = readline.createInterface({
+//   input: fs.createReadStream('./DataBase/photos.csv'),
+//   output: fs.createWriteStream('./DataBase/cleanPhotos.csv')
+// });
 
-//   .on('end',()=>{
-//     console.log(' products');
-// console.log('--------------------------');
-//     for(i =0; i < products.length; i++){
-//       var number = parseInt(products[i][' default_price'])
+// const readStyles = readline.createInterface({
+//   input: fs.createReadStream('./DataBase/styles.csv'),
+//   output: fs.createWriteStream('./DataBase/cleanStyles.csv')
+// })
+
+// const readSkus = readline.createInterface({
+//   input: fs.createReadStream('./DataBase/skus.csv'),
+//   output: fs.createWriteStream('./DataBase/cleanSkus.csv')
+// })
+
+
+
+
+// works
+// readSkus.on('line', (line)=>{
+//   var pass = true
+//   var arr = line.split(',')
+//   if (isNaN(parseInt(arr[0]))){
+//     pass = false
+//   }
+//   if (isNaN(parseInt(arr[1]))){
+//     pass = false
+//   }
+//   if (isNaN(parseInt(arr[3]))){
+//     pass = false
+//   }
+//   if(arr.length > 4){
+//     pass = false
+//   }
+//   if(pass === true ){
+//     readSkus.output.write(line + '\n' )
+//   }
+// }).on('end',()=>{
+//   console.log('completed Skus')
+// })
+
+/// works
+// readStyles.on('line', (line)=>{
+//   arr = line.split(',')
+//   pass = true;
+//   if(isNaN(parseInt(arr[0]))  ){
+//     pass = false
+//   }
+//   if(isNaN(parseInt(arr[1])) ){
+//     pass = false
+//   }
+//   if(isNaN(parseInt(arr[4])) ){
+//     pass = false
+//   }
+//   if(arr[5] !== '0' ){
+//     if(arr[5] !== '1'){
+//     pass = false
+//     }
+//   }
+//   if(arr[4] !== 'null' && isNaN(parseInt(arr[5])) ){
+//     pass = false
+//   }
+//   if(arr.length > 6 ){
+//     pass = false
+//   }
+//   if(pass === true){
+//     readStyles.output.write(line + '\n')
+//   }
+// }).on('end',()=>{
+//   console.log('completed Styles')
+// })
+
+
+// this works
+// readPhotos.on('line', (line) => {
+//     var arr = line.split(",")
+//     var pass = true;
+
+//     // check the data has a string at the end
+//    if(arr[3][0] !== '"' ){
+//       arr[3] =  '"'+arr[3]
+//    }
+//    if( arr[3][arr[3].length-1] !== '"'){
+//     arr[3] =  arr[3] + '"'
+
+//   }
+//   if(arr[2][0] !== '"' ){
+//     arr[2] =  '"'+arr[2]
+
+//  }
+//  if( arr[2][arr[2].length-1] !== '"'){
+//   arr[2] =  arr[2] + '"'
+
+// }
+//   if(isNaN(parseInt(arr[1]))){
+//     pass = false;
+//   }
+//   if(isNaN(parseInt(arr[0]))){
+//     pass = false;
+//   }
+
+//     if(isValidURL(arr[2]) === false){
+//       part1= arr[2].substring(0,1);
+//       part2 =  arr[2].substring(2,arr[2].length);
+//       arr[2]= part1 + part2
+//   }
+//   if(isValidURL(arr[3]) === false){
+
+//     part1= arr[3].substring(0,1);
+//     part2 =  arr[3].substring(2,arr[3].length);
+//     arr[3]=  part1 + part2
+
+//   }
+//   line = arr.join(',')
+// if(pass === true){
+//   readPhotos.output.write(line + '\n') // add a new line
+// }
+
+// }).on('end',()=>{
+//   console.log('completed photos')
+// });
+
+
+
+
+  /// this works
+// fs.createReadStream('./DataBase/product.csv').pipe(csv({})).on('data', (data)=>{
+//   var str = '';
+//   var boolean = true
+//   for(var key in data){
+//     var value = data[key]
+//     if(key === ' default_price'){
+//       var number = parseInt(value)
 //       if(isNaN(number)){
-//         var bew = products[i][' default_price'].match(/[0-9]+/g)
-//         products[i][' default_price'] = bew[0]
+//       var number = value.match(/[0-9]+/g);
+//       value = number
 //       }
 //     }
-
-//   })
-
-
-//   fs.createReadStream('./DataBase/styles.csv').pipe(csv({})).on('data', (data)=>{
-//     styles.push(data)}).on('end',()=>{
-
-//       console.log('styles ');
-// console.log('--------------------------');
-
-//       for(var i = 0; i < styles.length;i++){
-//         var number = parseInt(styles[i].sale_price)
-//         if(isNaN(number) && styles[i].sale_price!== 'null'){
-//           failed.push(styles[i])
-//         }
+//     if(boolean){
+//       if(key !== ' default_price' && key !== 'id' ){
+//         str += '"'+value+'"';
+//         boolean = false;
+//       }else{
+//         str += value;
+//     boolean = false;
 //       }
-//      console.log(failed)
-
+//   }else {
+//     if(key !== ' default_price' && key !== 'id' ){
+//       str += ',"' + value +'"';
+//     }else {
+//       str += ',' + value;
+//     }
+//   }
+//   }
+//   fs.appendFile('./DataBase/cleanProducts.csv',str + '\n',(err)=>{
+//     if(err){
+//       console.log(err)
+//     }else{
+//     }
 //     })
+// }).on('end',()=>{
+//   console.log('completed products')
+// })
 
 
 
-
-
-    // var obj={}
-    // obj.table = [ {id: '11',
-    //  name: 'Air Minis 250',
-    //  slogan: 'Full court support',
-    //  description: 'This optimized air cushion pocket reduces impact but keeps a perfect balance underfoot.',
-    //  category: 'Basketball Shoes',
-    //  default_price: '49'},{id: '12',
-    //  name: 'Air Minis 250',
-    //  slogan: 'Full court support',
-    //  description: 'This optimized air cushion pocket reduces impact but keeps a perfect balance underfoot.',
-    //  category: 'Basketball Shoes',
-    //  default_price: '49'} ]
-    //  var string = [];
-    //  for(var i = 0; i < obj.table.length; i++){
-    //    var str = '';
-    //    var first = true;
-    //    for(var key in obj.table[i]){
-    //      if(first){
-    //       str += obj.table[i][key] + '';
-    //       first = false;
-    //      }else {
-    //     str +=  ', ' + obj.table[i][key]
-    //      }
-    //    }
-    //    string.push(str);
-    //  }
-    //  let test = string.join('\n')
-    // fs.writeFile('test.txt',test,(err)=>{
-    // if(err){
-    //   console.log(err)
-    // }else{
-    //   console.log('completed')
-    // }
-    // })
-
-
-
-
-    // console.log('sku ');
-    // console.log('--------------------------');
-    //   fs.createReadStream('./DataBase/skus.csv').pipe(csv({})).on('data', (data)=>{
-    //     console.log(data)})
-
-    //     .on('end',()=>{
-    //      console.log(results2)
-
-    //     })
--- schema.sql
+++ schema.sql
@@ -6,7 +6,7 @@ CREATE TABLE Product (
   product_id INTEGER AUTO_INCREMENT,
   name VARCHAR(255) NOT NULL,
   slogan VARCHAR(255) NOT NULL,
-  description VARCHAR(1000) NOT NULL,
+  description TEXT NOT NULL,
   category VARCHAR(255) NOT NULL,
   default_price INTEGER NOT NULL,
   PRIMARY KEY (product_id)
@@ -16,7 +16,7 @@ CREATE TABLE Features (
   feature_id INTEGER AUTO_INCREMENT,
   product_id INTEGER NOT NULL,
   feature VARCHAR(255) NOT NULL,
-  value VARCHAR(255) NOT NULL,
+  value VARCHAR(255),
   PRIMARY KEY (feature_id),
   FOREIGN KEY (product_id) REFERENCES Product(product_id)  ON DELETE CASCADE
 );
@@ -25,8 +25,8 @@ CREATE TABLE Styles (
   style_id INTEGER AUTO_INCREMENT,
   product_id INTEGER NOT NULL,
   name VARCHAR(255) NOT NULL,
-  original_price INTEGER NOT NULL,
-  sale_price INTEGER DEFAULT NULL,
+  sale_price VARCHAR(255) DEFAULT NULL,
+  original_price VARCHAR(255) NOT NULL,
   default_style BINARY DEFAULT 0 NOT NULL,
   PRIMARY KEY (style_id),
   FOREIGN KEY (product_id) REFERENCES Product(product_id) ON DELETE CASCADE
@@ -44,8 +44,8 @@ CREATE TABLE Skus (
  CREATE TABLE Photo (
   photo_id INTEGER AUTO_INCREMENT,
   style_id INTEGER NOT null,
-  url VARCHAR(2048) NOT NULL,
-  thumbnail_url VARCHAR(2048) NOT NULL,
+  url VARCHAR(255) NOT NULL,
+  thumbnail_url VARCHAR(255) NOT NULL,
   PRIMARY KEY (photo_id),
   FOREIGN KEY (style_id) REFERENCES Styles(style_id) ON DELETE CASCADE
  );
@@ -54,6 +54,7 @@ CREATE TABLE Skus (
   id INTEGER AUTO_INCREMENT,
   product_id INTEGER NOT NULL,
   related_id INTEGER NOT NULL,
+  UNIQUE(product_id,related_id),
   FOREIGN KEY (related_id) REFERENCES Product(product_id)  ON DELETE CASCADE,
   FOREIGN KEY (product_id) REFERENCES Product(product_id)  ON DELETE CASCADE,
   primary key(id)
